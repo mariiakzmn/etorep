1161151f79c7c5cc781a473145afa285a990e332
mariiakzmn <67392761+mariiakzmn@users.noreply.github.com>
2021-09-22 14:43:15 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,19 @@
+from connectors import ConnectorRouter
+import pandas as pd
+import datetime
 
+API_KEY = ''
+SECRET_KEY = ''
+balance = 100
+client = ConnectorRouter(exchange='Binance', section='SPOT').init_connector(API_KEY, SECRET_KEY)
+klines = client.get_klines("ETHUSDT", '1M')
+prices = [dict['close'] for dict in klines]
+first_price = prices[0]
+timestamp = [dict['openTime'] for dict in klines]
+
+blnc = [balance * (cur_price / first_price) for cur_price in prices]
+df = pd.DataFrame(np.array([timestamp, blnc),
+                   columns=['timestamp', 'balance'])
+                            
+                     
+                            
